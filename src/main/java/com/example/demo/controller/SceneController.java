6a1f82092a6859e1d2297f3723e5a1e408dbcdfb
MUK3Y <162259598+MUK3Y@users.noreply.github.com>
2024-12-04 01:54:13 +0800

    Added color to Play and Quit when hovered and increased size for the game title and repositioned it

--- src/main/java/com/example/demo/controller/SceneController.java
+++ src/main/java/com/example/demo/controller/SceneController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -20,24 +21,40 @@ public class SceneController {
     @FXML
     private Button quitButton;
 
-    // Reference to the main stage
+
     private Stage stage;
 
-    // Set the stage reference
+
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
     @FXML
     public void initialize() {
-        // Event handlers for buttons
+
         startGameButton.setOnAction(this::handleStartGame);
+        startGameButton.setOnMouseEntered(event -> {
+            startGameButton.setTextFill(Color.DARKRED);
+        });
+
+        startGameButton.setOnMouseExited(event -> {
+            startGameButton.setTextFill(Color.WHITE);
+        });
+
         quitButton.setOnAction(this::handleQuit);
+        quitButton.setOnMouseEntered(event -> {
+            quitButton.setTextFill(Color.DARKRED);
+        });
+
+        quitButton.setOnMouseExited(event -> {
+            quitButton.setTextFill(Color.WHITE);
+        });
+
     }
 
     private void handleStartGame(ActionEvent event) {
         try {
-            // Start the game by calling the Controller's launchGame method
+
             new Controller(stage).launchGame();
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,8 +62,6 @@ public class SceneController {
     }
 
     private void handleQuit(ActionEvent event) {
-        // Exit the application
-        System.out.println("Exiting the game...");
         stage.close();
 }
 }
