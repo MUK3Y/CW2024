bd159329d2b629e91094878124c445260d589b7f
MUK3Y <162259598+MUK3Y@users.noreply.github.com>
2024-12-03 22:26:46 +0800

    Added a main menu, with play and quit button, that redirects respectively

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
-import java.lang.reflect.InvocationTargetException;
 
+import com.example.demo.LevelParent;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,16 +13,24 @@ public class Main extends Application {
 	private static final int SCREEN_WIDTH = 1300;
 	private static final int SCREEN_HEIGHT = 750;
 	private static final String TITLE = "Sky Battle";
-	private Controller myController;
+
 
 	@Override
-	public void start(Stage stage) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public void start(Stage stage) throws Exception {
+
 		stage.setTitle(TITLE);
 		stage.setResizable(false);
 		stage.setHeight(SCREEN_HEIGHT);
 		stage.setWidth(SCREEN_WIDTH);
 		LevelParent.BGplaySound("/Music/BGM.wav");
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/images/MainMenu.fxml"));
+		Parent root = loader.load();
+		Scene mainMenuScene = new Scene(root);
+		stage.setScene(mainMenuScene);
+		SceneController controller = loader.getController();
+		controller.setStage(stage);
+		stage.show();
 
 
 	}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.demo to javafx.fxml;
+    opens com.example.demo.controller to javafx.fxml;
     exports com.example.demo.controller;
+
 }
